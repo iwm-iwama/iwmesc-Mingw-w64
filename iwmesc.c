9a23a87d924a09ad78444ce78c83607a53fd02d5
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2025-02-06 08:19:52 +0900

    Add files via upload

--- iwmesc.c
+++ iwmesc.c
@@ -1,7 +1,7 @@
 //------------------------------------------------------------------------------
-#define   IWM_COPYRIGHT       "(C)2023-2024 iwm-iwama"
+#define   IWM_COPYRIGHT       "(C)2023-2025 iwm-iwama"
 #define   IWM_FILENAME        "iwmesc"
-#define   IWM_UPDATE          "20241216"
+#define   IWM_UPDATE          "20250202"
 //------------------------------------------------------------------------------
 #include "lib_iwmutil2.h"
 
@@ -152,8 +152,8 @@ print_version()
 	P1(IESC_STR2);
 	LN(80);
 	P1(
-		" " IWM_COPYRIGHT "\n"
-		"    " IWM_FILENAME "_" IWM_UPDATE " + " LIB_IWMUTIL_FILENAME "\n"
+		"\033[2G"	IWM_COPYRIGHT	"\n"
+		"\033[5G"	IWM_FILENAME	"_"	IWM_UPDATE	" + "	LIB_IWMUTIL_FILENAME	"\n"
 	);
 	LN(80);
 	P1(IESC_RESET);
@@ -164,41 +164,35 @@ print_help()
 {
 	print_version();
 	P1(
-		IESC_TITLE1	" エスケープシーケンスを含むコマンドを実行／出力 " IESC_RESET "\n\n"
-		IESC_STR1	"    " IWM_FILENAME
-		IESC_OPT2	" [Option]"
-		IESC_OPT1	" [Str]\n\n\n"
-		IESC_LBL1	" (例１)"
-		IESC_STR1	" 引数渡し\n"
-					"    " IWM_FILENAME
-		IESC_OPT1	" \"\\033[92m\" \"テキスト\\n\" \"\\033[96m表示\" \"\\033[0m\\n\"\n\n"
-		IESC_LBL1	" (例２)"
-		IESC_STR1	" パイプ渡し\n"
-		IESC_OPT1	"    ls |"
-		IESC_STR1	" " IWM_FILENAME "\n\n"
-		IESC_LBL1	" (例３)"
-		IESC_STR1	" -script 直接実行\n"
-					"    " IWM_FILENAME
-		IESC_OPT2	" -script"
-		IESC_OPT1	" python -c \"print('\\033[92mテキスト\\n\\033[96m表示\\033[0m\\n')\"\n\n"
-		IESC_LBL1	" (例４)"
-		IESC_STR1	" -script ファイルから実行\n"
-					"    " IWM_FILENAME
-		IESC_OPT2	" -script"
-		IESC_STR1   "\033[20G\033[44m foo \033[49m\n"
-					"\033[20G\033[44m                                                    \033[49m\n"
-					"\033[20G\033[44m  #!/usr/bin/env python                             \033[49m\n"
-					"\033[20G\033[44m  #coding:utf-8                                     \033[49m\n"
-					"\033[20G\033[44m  print('\\033[92mテキスト\\n\\033[96m表示\\033[0m\\n')  \033[49m\n"
-					"\033[20G\033[44m                                                    \033[49m\n"
-		IESC_STR2	"\033[20G ※foo以降の引数は\"fooの引数\"になる\n\n"
-	);
-	P1(
-		IESC_OPT2	" [Option]\n"
-		IESC_OPT21	"    -script Str | -s Str\n"
-		IESC_STR1	"        Strをスクリプトとして実行\n"
-					"        Strがスクリプトファイルのとき（1行目）#! のインタプリタを使用\n"
-		IESC_STR2	"        ※出力コードはスクリプト基準\n\n"
+		"\033[1G"	IESC_TITLE1	" エスケープシーケンスを含むコマンドを実行／出力 "	IESC_RESET	"\n"
+		"\n"
+		"\033[5G"	IESC_STR1	IWM_FILENAME	IESC_OPT2	" [Option]"	IESC_OPT1	" [Str]"	"\n"
+		"\n"
+		"\033[2G"	IESC_LBL1	"(例１)"	IESC_STR1	" 引数渡し"	"\n"
+		"\033[5G"	IESC_STR1	IWM_FILENAME	IESC_OPT1	" \"\\033[92m\" \"テキスト\\n\" \"\\033[96m表示\" \"\\033[0m\\n\""	"\n"
+		"\n"
+		"\033[2G"	IESC_LBL1	"(例２)"	IESC_STR1	" パイプ渡し"	"\n"
+		"\033[5G"	IESC_OPT1	"ls | "	IESC_STR1	IWM_FILENAME	"\n"
+		"\n"
+		"\033[2G"	IESC_LBL1	"(例３)"	IESC_STR1	" -script 直接実行"	"\n"
+		"\033[5G"	IWM_FILENAME	IESC_OPT2	" -script"	IESC_OPT1	" python -c \"print('\\033[92mテキスト\\n\\033[96m表示\\033[0m\\n')\""	"\n"
+		"\n"
+		"\033[2G"	IESC_LBL1	"(例４)"	IESC_STR1	" -script スクリプトファイルから実行"	"\n"
+		"\033[5G"	IWM_FILENAME	IESC_OPT2	" -script "	IESC_STR2	"\033[44m"	" foo "	"\033[49m"	" ..."	"\n"
+		IESC_STR2
+		"\033[9G"	"\033[44m"	"                                                    "	"\033[49m"	"\n"
+		"\033[9G"	"\033[44m"	"  #!/usr/bin/env python                             "	"\033[49m"	"\n"
+		"\033[9G"	"\033[44m"	"  #coding:utf-8                                     "	"\033[49m"	"\n"
+		"\033[9G"	"\033[44m"	"  print('\\033[92mテキスト\\n\\033[96m表示\\033[0m\\n')  "	"\033[49m"	"\n"
+		"\033[9G"	"\033[44m"	"                                                    "	"\033[49m"	"\n"
+		"\033[9G"	IESC_STR1	"※ ... は fooスクリプトファイルの引数 になる"	"\n"
+		"\n"
+		"\033[2G"	IESC_OPT2	"[Option]"	"\n"
+		"\033[5G"	IESC_OPT21	"-script Str | -s Str"	"\n"
+		"\033[9G"	IESC_STR1	"Strをスクリプトとして実行"	"\n"
+		"\033[9G"	IESC_STR1	"Strがスクリプトファイルのとき シバン行(*1)のインタプリタを使用"	"\n"
+		"\033[9G"	IESC_STR2	"(*1) shebang／#!から始まる１行目"	"\n"
+		"\n"
 	);
 	P1(IESC_STR2);
 	LN(80);
