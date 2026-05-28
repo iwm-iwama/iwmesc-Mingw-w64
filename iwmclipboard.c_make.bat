756f3405b4cb4df2e9e6f845a9c757f7216a9a4f
iwm-iwama <52396966+iwm-iwama@users.noreply.github.com>
2026-05-28 17:53:07 +0900

    Add files via upload

---
+++ iwmclipboard.c_make.bat
@@ -0,0 +1,50 @@
+@echo off
+cls
+
+:: "ベースファイル名" + "_make.bat"
+set batch=%~nx0
+set src=%batch:_make.bat=%
+set fn=
+for /f "delims=. tokens=1,2" %%i in ("%src%") do (
+	set fn=%%i%
+)
+set fn_exe=%fn%.exe
+set cc=gcc.exe -std=c23
+set lib=lib_iwmutil2.a
+set op_link=-Os -Wall -Wextra -Wformat=2 -lgdi32 -luser32 -lshlwapi
+
+:: Assembler
+	if exist "%fn%_old.s" (
+		rm "%fn%_old.s"
+	)
+	if exist "%fn%.s" (
+		mv "%fn%.s" "%fn%_old.s"
+	)
+::	for %%s in (%src%) do (
+::		%cc% %%s -S %op_link%
+::	)
+::	ls -la *.s
+::	echo.
+
+:: Make
+	echo --- Make ------------------------------------------
+	%cc% %src% %lib% %op_link% -o %fn_exe%
+	strip %fn_exe%
+	echo.
+
+:: Dump
+::	cp -f %fn%.objdump %fn%.objdump.old
+::	objdump -d -s %fn_exe% > %fn%.objdump
+::	echo.
+
+:: Test
+	pause
+	chcp 65001
+	cls
+
+	%fn_exe%
+
+:: Quit
+	echo.
+	pause
+	exit
